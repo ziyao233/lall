c8d01cc2d688b8ba082f4d7f11fac3b4b0cf1cf0
st127 <tswuyin_st127@163.com>
2022-01-14 23:27:00 +0800

    feat: Add basic structure of lall.Stream

---
+++ src/stream.c
@@ -0,0 +1,80 @@
+/*
+	lall
+	File:/src/stream.c
+	Date:2022.01.14
+	By MIT License.
+	Copyright (c) 2022 lall developers.All rights reserved.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#include<lua.h>
+#include<lauxlib.h>
+
+#include"stream.h"
+
+Lall_Stream *lall_stream_new(lua_State *state,int fd)
+{
+	Lall_Stream *stream = (Lall_Stream*)
+		lua_newuserdata(state,sizeof(Lall_Stream));
+
+	luaL_getmetatable(state,"lall.Stream");
+	lua_setmetatable(state,-2);
+
+	stream->fd = fd;
+
+	return stream;
+}
+
+static int interface_stream(lua_State *state)
+{
+	int fd = (int)luaL_checkinteger(state,1);
+	lall_stream_new(state,fd);
+	return 1;
+}
+
+/*
+	The stream needs being on the top of the stack.
+*/
+int lall_stream_get_fd(lua_State *state)
+{
+	lua_pushinteger(state,((Lall_Stream*)
+				luaL_checkudata(state,-1,
+						"lall.Stream"))
+			       ->fd);
+	return 1;
+}
+
+static const struct luaL_Reg streamPrototype[] = {
+		{
+			"get_fd",	lall_stream_get_fd,
+		},
+		{
+			NULL,		NULL
+		}
+	};
+
+static const struct luaL_Reg modStream[] = {
+		{
+			"Stream",	interface_stream
+		},
+		{
+			NULL,		NULL
+		}
+	};
+
+
+/*
+	The main chunk of module lall.stream
+*/
+int luaopen_lall_stream(lua_State *state)
+{
+	luaL_newmetatable(state,"lall.Stream");
+	luaL_newlib(state,streamPrototype);
+	lua_setfield(state,-2,"__index");
+
+	luaL_newlib(state,modStream);
+	return 1;
+}
---
+++ src/stream.h
@@ -0,0 +1,21 @@
+/*
+	lall
+	File:/src/stream.h
+	Date:2022.01.14
+	By MIT License.
+	Copyright (c) 2022 lall developers.All rights reserved.
+*/
+
+#ifndef __LALL_STREAM_H_INC__
+#define __LALL_STREAM_H_INC__
+
+#include<lua.h>
+#include<lauxlib.h>
+
+typedef struct {
+	int fd;
+}Lall_Stream;
+
+Lall_Stream *lall_stream_new(lua_State *state,int fd);
+
+#endif	// __LALL_STREAM_H_INC__
